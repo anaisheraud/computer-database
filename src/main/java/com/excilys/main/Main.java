522ccdce9e1087d34fba430b329cc07b34354881
anaisheraud <anaisheraud@outlook.fr>
2020-07-16 16:23:43 +0200

    Sprint 5 - JDBC Template

--- src/main/java/com/excilys/main/Main.java
+++ src/main/java/com/excilys/main/Main.java
@@ -5,7 +5,6 @@ import com.excilys.cli.CommandLineInterface;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		CommandLineInterface cli = new CommandLineInterface();
 		
